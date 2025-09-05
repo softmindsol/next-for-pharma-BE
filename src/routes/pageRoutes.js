90cb0c0977064de2713783c549f9e7ab395a9a37
ahmedmustafa007 <ahmedmustafa@softmindsol.com>
2025-09-06 03:27:48 +0500

    Add CORS middleware to pageRoutes.js for enhanced request handling

--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
 import { getPage, updatePage } from "../controllers/pageController.js";
 import { protect } from "../middleware/auth.js";
+import cors from "cors";
 
 const router = express.Router();
 
+router.use(
+  cors({
+    origin: "*", // or specify your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 router.get("/:slug", getPage);
 router.put("/:slug", protect, updatePage);
 
