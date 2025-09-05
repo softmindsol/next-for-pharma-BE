ac3e4f781c6fed5cf468a4685ed946a98ff7bfa4
ahmedmustafa007 <ahmedmustafa@softmindsol.com>
2025-09-06 02:51:06 +0500

    Enhance CORS configuration in server.js to allow requests from any origin.

--- src/server.js
+++ src/server.js
@@ -8,7 +8,11 @@ import pageRoutes from "./routes/pageRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 mongoose
