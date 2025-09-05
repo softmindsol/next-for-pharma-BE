e574651c27c69fd0a40fc2e84d7de111ad9d865c
ahmedmustafa007 <ahmedmustafa@softmindsol.com>
2025-09-06 02:54:18 +0500

    Update CORS configuration in server.js to specify allowed methods and headers.

--- src/server.js
+++ src/server.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(
   cors({
     origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
